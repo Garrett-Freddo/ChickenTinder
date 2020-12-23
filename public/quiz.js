efa3cbca865fc44be731c06430e33f5ca4deca62
kevinli <kevkev00@gmail.com>
2020-12-22 23:19:29 -0600

    start

---
+++ public/quiz.js
@@ -0,0 +1,9 @@
+const CONSTANTS = document.addEventListener("DOMContentLoaded", event => {
+    app    = firebase.app();
+    db     = firebase.firestore();
+    userDB = db.collection('users');
+})
+
+$("#resuilts-btn").click(function() {
+    
+})
